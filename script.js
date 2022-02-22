2346eec052d4c226de8e5e614455eed647457447
Till-Taeubrich <Till.taeubrich@gmail.com>
2022-02-22 19:58:37 +1100

    fundamental objects added

---
+++ script.js
@@ -0,0 +1,20 @@
+const Gameboard = (function(){
+  const gameboard = [];
+
+  // const crossFields = [];
+  // const circleFields = [];
+  
+  // return {
+  //   crossFields = crossFields,
+  //   circleFields = circleFields,
+  // }
+})
+
+// need to be checked and factory syntax needs to be added to notion
+function playerFactory (name){
+  let roundsWon = 0;
+  return {name, roundsWon};
+}
+
+const gameFlow = (function(){
+})
